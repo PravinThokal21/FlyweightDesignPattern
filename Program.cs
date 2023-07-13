1700fd8e88e07db9685bda94bbc2fbe6c99fd447
Pravin Thokal <thokalpravinkumar@gmail.com>
2023-07-13 11:56:10 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using FlyweightDesignPattern;
+
+Console.WriteLine("Flyweight Design Pattern");
+
+ShapeObjectFactory sof = new ShapeObjectFactory();
+
+IShape shape = sof.GetShape("Rectangle");
+shape.Print();
+shape = sof.GetShape("Rectangle");
+shape.Print();
+shape = sof.GetShape("Rectangle");
+shape.Print();
+
+shape = sof.GetShape("Circle");
+shape.Print();
+shape = sof.GetShape("Circle");
+shape.Print();
+shape = sof.GetShape("Circle");
+shape.Print();
+
+int NumObjs = sof.TotalObjectsCreated;
+Console.WriteLine("\nTotal No of Objects created = {0}", NumObjs);
+Console.ReadKey();
+
---
+++ ShapeObjectFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightDesignPattern
+{
+    internal class ShapeObjectFactory
+    {
+        Dictionary<string, IShape> shapes = new Dictionary<string, IShape>();
+
+        public int TotalObjectsCreated
+        {
+            get { return shapes.Count; }
+        }
+
+        public IShape GetShape(string ShapeName)
+        {
+            IShape shape = null;
+            if (shapes.ContainsKey(ShapeName))
+            {
+                shape = shapes[ShapeName];
+            }
+            else
+            {
+                switch (ShapeName)
+                {
+                    case "Rectangle":
+                        shape = new Rectangle();
+                        shapes.Add("Rectangle", shape);
+                        break;
+                    case "Circle":
+                        shape = new Circle();
+                        shapes.Add("Circle", shape);
+                        break;
+                    default:
+                        throw new Exception("Factory cannot create the object specified");
+                }
+            }
+            return shape;
+        }
+    }
+}
